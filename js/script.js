4a946a0bdea15c28fa2818b356bd34c71dabd644
Andrey <gavrilovandruysha@list.ru>
2022-03-17 20:02:17 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+const peronalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-alert('привет');
+peronalMovieDb.movies[a] = b;
+peronalMovieDb.movies[c] = d;
+
+console.log(peronalMovieDb);
